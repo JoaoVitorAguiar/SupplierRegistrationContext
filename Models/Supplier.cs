329240c77ecc3bb7b5c529215dffc2e1f37d26de
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-08-25 00:52:06 -0300

    feat: add Supplier entity

---
+++ Models/Supplier.cs
@@ -0,0 +1,11 @@
+﻿namespace SupplierRegistrationContext.Models;
+
+public class Supplier
+{
+    public int Id { get; set; } 
+    public string Name { get; set; }
+    public string CNPJ { get; set; }
+    public string CEP { get; set; }
+
+    public Specialty Specialty { get; set; }
+}
