5f29c219f4c5eec80d8b82cc69e55500ce9cdd00
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-08-25 08:23:46 -0300

    feat: add data annotation in Specialties

--- Models/Specialty.cs
+++ Models/Specialty.cs
@@ -1,8 +1,17 @@
-﻿namespace SupplierRegistrationContext.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace SupplierRegistrationContext.Models;
+
+[Table("Specialties")]
 public class Specialty
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [MinLength(2)]
     public string Name { get; set; }
 
     public IList<Supplier> Suppliers { get; set; }
