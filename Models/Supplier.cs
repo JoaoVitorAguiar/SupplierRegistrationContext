3024c3f648eaf28ebe541f0410508a12936a46b2
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-08-25 01:15:34 -0300

    feat: add data annotation in Supplier

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -1,12 +1,29 @@
-﻿namespace SupplierRegistrationContext.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace SupplierRegistrationContext.Models;
+
+[Table("Suppliers")]
 public class Supplier
 {
-    public int Id { get; set; } 
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [MinLength(2)]
     public string Name { get; set; }
+
+    [Required]
     public int CNPJ { get; set; }
+
+    [Required]
     public int CEP { get; set; }
+
+    [Required]
+    [StringLength(255)]
     public string Address { get; set; }
 
+    [Required]
     public Specialty Specialty { get; set; }
 }
