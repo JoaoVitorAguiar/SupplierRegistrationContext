5d9c5c37575b12aece787ee33ed818392713d99e
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-08-25 08:31:44 -0300

    chore: add packages for DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SupplierRegistrationContext.Data;
+
+public class DataContext : DbContext
+{
+
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+}
