dfdfa1e9842b20e8936421208ec8032431e19ad7
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-08-25 01:02:32 -0300

    feat: add Specialty entity

---
+++ Models/Specialty.cs
@@ -0,0 +1,9 @@
+﻿namespace SupplierRegistrationContext.Models;
+
+public class Specialty
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public IList<Supplier> Suppliers { get; set; }
+}
