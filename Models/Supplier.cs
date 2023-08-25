5cddc4366d112842bcdad158be9b31fc9f2e9116
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-08-25 01:03:44 -0300

    fix: change the model Supplier
    change the numertic props

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -4,8 +4,9 @@ public class Supplier
 {
     public int Id { get; set; } 
     public string Name { get; set; }
-    public string CNPJ { get; set; }
-    public string CEP { get; set; }
+    public int CNPJ { get; set; }
+    public int CEP { get; set; }
+    public string Address { get; set; }
 
     public Specialty Specialty { get; set; }
 }
