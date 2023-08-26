4b120fad2b899ede67f231348e035a4d874ddc75
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-08-26 19:23:54 -0300

    feat: add list of the supllies

---
+++ Pages/Supplier/List.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupplierRegistrationContext.Data;
+using SupplierRegistrationContext.Models;
+
+namespace SupplierRegistrationContext.Pages.Supplier
+{
+    public class ListModel : PageModel
+    {
+        private readonly SupplierRegistrationContext.Data.DataContext _context;
+
+        public ListModel(SupplierRegistrationContext.Data.DataContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Models.Supplier> Supplier { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Suppliers != null)
+            {
+                Supplier = await _context.Suppliers.ToListAsync();
+            }
+        }
+    }
+}
