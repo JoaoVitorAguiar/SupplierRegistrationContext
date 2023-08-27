17ec5f48855fc5a6c99db4366fd86ce1d6a18085
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-08-26 21:46:22 -0300

    feat: add service AddressFromCep

---
+++ Services/AddressFromCep.cs
@@ -0,0 +1,20 @@
+﻿namespace SupplierRegistrationContext.Services;
+
+using SupplierRegistrationContext.ViewModels;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public static class AddressFromCep
+{
+    public static async Task<string> GetJson(string cep)
+    {
+        var url = $"https://viacep.com.br/ws/{cep}/json/";
+        HttpClient cliente = new HttpClient();
+        HttpResponseMessage resposta = await cliente.GetAsync(url);
+        resposta.EnsureSuccessStatusCode();
+
+        var jsonString = await resposta.Content.ReadAsStringAsync();
+        var address = JsonSerializer.Deserialize<AddressFromCepViewModel>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        return address.ToString();
+    }
+}
---
+++ ViewModels/AddressFromCepViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace SupplierRegistrationContext.ViewModels;
+
+public class AddressFromCepViewModel
+{
+    [JsonProperty("cep")]
+    public string CEP { get; set; }
+    [JsonProperty("logradouro")]
+    public string Logradouro { get; set; }
+    [JsonProperty("complemento")]
+    public string Complemento { get; set; }
+    [JsonProperty("bairro")]
+    public string Bairro { get; set; }
+    [JsonProperty("localidade")]
+    public string Localidade { get; set; }
+    [JsonProperty("uf")]
+    public string UF { get; set; }
+    [JsonProperty("unidade")]
+    public string Unidade { get; set; }
+    [JsonProperty("ibge")]
+    public string IBGE { get; set; }
+    [JsonProperty("gia")]
+    public string Gia { get; set; }
+
+    public override string? ToString()
+    {
+        string enderecoFormatado = $"{Logradouro}";
+
+        if (!string.IsNullOrEmpty(Complemento))
+            enderecoFormatado += $", {Complemento}";
+
+        if (!string.IsNullOrEmpty(Bairro))
+            enderecoFormatado += $" - {Bairro}";
+
+        if (!string.IsNullOrEmpty(Localidade) && !string.IsNullOrEmpty(UF))
+            enderecoFormatado += $". {CEP}. {Localidade} - {UF}.";
+
+        return enderecoFormatado;
+    }
+    // Referência: http://www.visuarea.com.br/artigos/padronizacoes-corretas-de-informacoes-obrigatorias
+}
