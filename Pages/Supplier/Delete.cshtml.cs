c987c2e42144840418f286bb8098a65a49621d93
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-08-27 12:09:57 -0300

    feat: add delete page Supplier

---
+++ Pages/Supplier/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupplierRegistrationContext.Data;
+using SupplierRegistrationContext.Models;
+
+namespace SupplierRegistrationContext.Pages.Supplier
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupplierRegistrationContext.Data.DataContext _context;
+
+        public DeleteModel(SupplierRegistrationContext.Data.DataContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Models.Supplier Supplier { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Suppliers == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Suppliers.Include(x=>x.Specialty).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Supplier = supplier;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Suppliers == null)
+            {
+                return NotFound();
+            }
+            var supplier = await _context.Suppliers.FindAsync(id);
+
+            if (supplier != null)
+            {
+                Supplier = supplier;
+                _context.Suppliers.Remove(Supplier);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./List");
+        }
+    }
+}
